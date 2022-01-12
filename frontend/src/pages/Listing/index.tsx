48a46994189c6cd8ff7ad17ed718b584f38d0959
William Nogueira <william.nogueira02@gmail.com>
2022-01-12 10:28:31 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,42 @@
+import MovieCard from "componentes/MovieCard";
 import Pagination from "componentes/Pagination";
 
-function Listing(){
-    return(
-        <Pagination />
+function Listing() {
+    return (
+        <>
+            <Pagination />
 
-       )
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 colg-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                    <div className="col-sm-6 col-lg-4 colg-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                    <div className="col-sm-6 col-lg-4 colg-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                    <div className="col-sm-6 col-lg-4 colg-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                    <div className="col-sm-6 col-lg-4 colg-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                    <div className="col-sm-6 col-lg-4 colg-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
 }
 
 export default Listing;
