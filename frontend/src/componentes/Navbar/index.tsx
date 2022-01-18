e36028044bea2ee5123b36e9afd5fa301f3416b0
William Nogueira <william.nogueira02@gmail.com>
2022-01-17 22:24:10 -0300

    paginação

--- frontend/src/componentes/Navbar/index.tsx
+++ frontend/src/componentes/Navbar/index.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
                 <a href="https://github.com/WilliamNogueira01">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/William Nogueira</p>
                     </div>
                 </a>
             </div>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,28 +1,56 @@
+import axios from "axios";
 import MovieCard from "componentes/MovieCard";
 import Pagination from "componentes/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const [page, setPAge] = useState<MoviePage>({
+
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=score,desc`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPAge(data);
+            });
+
+    }, [pageNumber]);
+
+
+    const handlePAgeChange = (newPageNumber : number) =>{
+        setPageNumber(newPageNumber)
+    }
+
     return (
         <>
-            <Pagination />
-
+            <Pagination page ={page} onChange={handlePAgeChange}/>
             <div className="container">
                 <div className="row">
 
-                    {(() => {
-                        const options = [];
+                    {page.content.map(movie => (
+                        <div key={movie.id}className="col-sm-6 col-lg-4 colg-xl-3 mb-3">
+                            <MovieCard movie={movie} />
 
-                        for (let i = 0; i < 10; i++) {
-                            options.push(
-                            <div className="col-sm-6 col-lg-4 colg-xl-3 mb-3">
-                                <MovieCard />
-
-                            </div>)
-                        }
-
-                        return options;
-                    })()}
-
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
         </>
